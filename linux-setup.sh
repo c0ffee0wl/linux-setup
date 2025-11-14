69dc5bb3e54625ea0aa6564f48e18dfb5854f0e0
c0ffee0wl <c0ffee0wl@gmx.net>
2025-11-14 13:49:48 -0500

    style(prompt): Change default symbol to '@'

--- linux-setup.sh
+++ linux-setup.sh
@@ -746,7 +746,7 @@ if [ -n "$force_color_prompt" ]; then
 fi
 
 configure_prompt() {
-    prompt_symbol=㉿
+    prompt_symbol=@
     # Skull emoji for root terminal
     #[ "$EUID" -eq 0 ] && prompt_symbol=💀
     case "$PROMPT_ALTERNATIVE" in
